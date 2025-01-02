df93ed24d8e88e2cfef2aa709c210b12e62741ef
Alfazal007 <alfazal7777@gmail.com>
2025-01-03 04:45:46 +0530

    presigned url resources

---
+++ apiServer/controllers/controller.uploadNewImageToProject.go
@@ -0,0 +1,4 @@
+package controllers
+
+// TODO:: refer this to get presigned url https://cloudinary.com/documentation/generate_upload_signature_tutorial
+// https://cloudinary.com/documentation/delivery_url_signatures
